959b74b1f46e40a52c906da2faa3e4a76f119236
Artem Chernyshev <tenebrae@MacBook-Pro-Artem.local>
2022-06-06 08:51:11 +0300

    Some fixes

--- dicom-converter.sh
+++ dicom-converter.sh
@@ -1,13 +1,46 @@
 #!/bin/bash
+# debug line
+#trap 'echo "# $BASH_COMMAND";read' DEBUG
 
 # inspiration - http://debuirebrian.blogspot.com/2010/06/converting-dicom-images-to-jpeg-in.html
 
+# Progress bar - https://github.com/fearside/ProgressBar/
+
+# 1. Create ProgressBar function
+# 1.1 Input is currentState($1) and totalState($2)
+function ProgressBar {
+# Process data
+    let _progress=(${1}*100/${2}*100)/100
+    let _done=(${_progress}*4)/10
+    let _left=40-$_done
+# Build progressbar string lengths
+    _fill=$(printf "%${_done}s")
+    _empty=$(printf "%${_left}s")
+
+# 1.2 Build progressbar strings and print the ProgressBar line
+
+printf "\rProgress : [${_fill// /#}${_empty// /-}] ${_progress}%%"
+
+}
+
 # find all files in all subdirectories with dcm extension
 # and put it into list
+
+printf "Finding DICOM files in directory/subdirectories... "
 file_list=$(fd --extension dcm)
+
+printf "done.\n"
+# initialize counter
+number=1
+
+# number of iterations for progress bar
+_end=$(echo "$file_list" | wc -l)
+
+printf '\nConverting DICOM files...\n'
 
 # iterate over this list and convert each file
 for file in $file_list ; do
+
     # trim extension
     trimmed=$(basename $file .dcm)
 
@@ -17,40 +50,55 @@ for file in $file_list ; do
     # "+oj"    - write JPEG
     # "+Jq 90" - compression quality (actually, this is default value)
     # "+Wi 1"  - window number(no idea, what this means)
-    dcmj2pnm -d -v -im +oj +Jq 90 +Wi 1 $file $trimmed.jpg
 
+    #dcmj2pnm -d -v -im +oj +Jq 90 +Wi 1 $file $trimmed.jpg
+    dcmj2pnm +oj +Jq 90 +Wi 1 $file $trimmed.jpg
+
+    # convert file into json and parse needed data from it
     dcm2json $file | python ./json-process.py > $trimmed.json
+
+    # draw progress bar
+    ProgressBar ${number} ${_end}
+    # increment counter for progress bar
+    number=$[$number+1]
 done
 
-# THIS COULD BE REFACTORED, BUT I'M LAZY :(
+printf '\nCreating folders... '
+printf 'done.\n'
 
-# create folder for jpegs if it not exists
-if [ -d img ] ; then
-    echo "Folder already exists, continue..."
-else
-    mkdir img
-fi
+# create necessary folder, if they didn't exist
+for dir in \
+        "img" \
+        "JSON"
+do
+    if [ -d "$dir" ]; then
+        echo "${dir} folder already exists, continue..."
+    else
+        mkdir $dir
+    fi
+done
+
+printf '\nMoving stuff to folders... '
+printf 'done.\n'
 
 # find all jpegs and put it in folder
-file_list=$(fd --extension jpg)
+jpg_list=$(fd --extension jpg)
 
-for file in $file_list ; do
+# find all jsons and put it in folder
+json_list=$(fd --extension json)
+
+for file in $jpg_list ; do
     mv $file img
 done
 
-# create folder for json files
-if [ -d JSON ] ; then
-    echo "Folder already exists, continue..."
-else
-    mkdir JSON
-fi
-
-# find all jsons and put it in folder
-file_list=$(fd --extension json)
-
-for file in $file_list ; do
+for file in $json_list ; do
     mv $file JSON
 done
 
+printf '\nCreating csv... '
+printf 'done.\n'
+
 python json-to-csv.py
 rm -rf JSON
+
+printf '\nFinished!\n'
--- json-process.py
+++ json-process.py
@@ -1,7 +1,48 @@
 import sys, json
 
-def unpack_json(json_data):
+def extract_coordinates(json_data):
+    """
+    https://stackoverflow.com/questions/10241062/how-to-draw-scout-reference-lines-in-dicom
+
+    Example:
+
+        Group,Elem  VR Value                           Name of the tag
+    ---------------------------------------------------------------------
+    (0020,0032) DS [-249.51172\-417.51172\-821]  # ImagePositionPatient
+                    X0         Y0         Z0
+
+    (0020,0037) DS [1\0\0\0\1\0]                 # ImageOrientationPatient
+                    A B C D E F
+
+    scr = refers to the soruce image, i.e. the MR/CT slice
+    dst = refers to the destination image, i.e. the scout
+
+    pos_x, pos_y, pos_z = the X0, Y0, Z0 above
+    row_dircos_x, row_dircos_y, row_dircos_z = the A, B, C above
+    col_dircos_x, col_dircos_y, col_dircos_z = the D, E, F above
+    """
+
+    pos_x, pos_y, pos_z = json_data["00200032"]["Value"]
+    img_orientation = json_data["00200037"]["Value"]
+    row_dircos_x, row_dircos_y, row_dircos_z = img_orientation[:3]
+    col_dircos_x, col_dircos_y, col_dircos_z = img_orientation[3:]
+
     return {
+        "pos_x": pos_x,
+        "pos_y": pos_y,
+        "pos_z": pos_z,
+        "row_dircos_x": row_dircos_x,
+        "row_dircos_y": row_dircos_y,
+        "row_dircos_z": row_dircos_z,
+        "col_dircos_x": col_dircos_x,
+        "col_dircos_y": col_dircos_y,
+        "col_dircos_z": col_dircos_z
+    }
+
+def unpack_json(json_data):
+    coord = extract_coordinates(json_data)
+    return {
+            "UID": json_data["00080018"]["Value"][0],
             "Name": json_data["00100010"]["Value"][0]['Alphabetic'],
             "Sex": json_data["00100040"]["Value"][0],
             "Birth_date": json_data["00100030"]["Value"][0],
@@ -15,8 +56,17 @@ def unpack_json(json_data):
             "Slice_thickness": json_data["00180050"]["Value"][0],
             "Coil": json_data["00181250"]["Value"][0],
             "Matrix": json_data["00181310"]["Value"],
-            "Image_size": (json_data["00280010"]["Value"][0], json_data["00280011"]["Value"][0]),
-            "filename": json_data["00080018"]["Value"][0]
+            "Image_size": (json_data["00280010"]["Value"][0],
+                json_data["00280011"]["Value"][0]),
+            "pos_x": coord["pos_x"],
+            "pos_y": coord["pos_y"],
+            "pos_z": coord["pos_z"],
+            "row_dircos_x": coord["row_dircos_x"],
+            "row_dircos_y": coord["row_dircos_y"],
+            "row_dircos_z": coord["row_dircos_z"],
+            "col_dircos_x": coord["col_dircos_x"],
+            "col_dircos_y": coord["col_dircos_y"],
+            "col_dircos_z": coord["col_dircos_z"]
             }
 
 def main():
